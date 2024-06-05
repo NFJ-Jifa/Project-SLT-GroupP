e6ff630e7b4a4a602f98b5b1bac353ee68872825
Nathan Faustino <145617741+NFJ-Jifa@users.noreply.github.com>
2024-06-05 16:40:29 +0200

    Update Main.java
    Board (Place,Check if Empty)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+  public boolean isCellEmpty(int x, int y) {
+    return cells[x][y] == ' ';
+}
+
+public void place(int x, int y, char marker) {
+    if (isCellEmpty(x, y)) {
+        cells[x][y] = marker;
+    }
+}
 }
